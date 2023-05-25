45c2e50eb07137e7bb3326f9f23977622f3cb47b
Michael Comatas <mac776@scarletmail.rutgers.edu>
2023-05-25 13:36:16 -0400

    Added animated background that perserves aspect ratio based on device.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import { Container, Box, useColorModeValue } from '@chakra-ui/react'
+import { Container, Box, useColorModeValue, chakra, shouldForwardProp } from '@chakra-ui/react'
 import Head from 'next/head'
 import Layout from '../components/layouts/main'
+import { motion, isValidMotionProp, easeInOut } from 'framer-motion'
+import Image from 'next/image'
 
 const Page = () => {
     return (
